2ebf2eb0b65a4e5c8546e5e7c2eac8ca24500097
Hafrún <hah78@hi.is>
2017-05-24 12:40:25 +0000

    Breyta database tengingum

--- wp-config.php
+++ wp-config.php
@@ -20,16 +20,16 @@
 
 // ** MySQL settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
-define('DB_NAME', 'valkostur');
+define('DB_NAME', 'heroku_1c468420a264ace');
 
 /** MySQL database username */
-define('DB_USER', 'valkostur');
+define('DB_USER', 'b1d4a8874d4b99');
 
 /** MySQL database password */
-define('DB_PASSWORD', 'einsikokkur');
+define('DB_PASSWORD', '4ac90e9f');
 
 /** MySQL hostname */
-define('DB_HOST', 'localhost');
+define('DB_HOST', 'us-cdbr-iron-east-03.cleardb.net');
 
 /** Database Charset to use in creating database tables. */
 define('DB_CHARSET', 'utf8mb4');
